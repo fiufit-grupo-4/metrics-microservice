c8039d804956f88ba7a33a09e8a458924746d5bd
Martina Ágata <mpanetta@fi.uba.ar>
2023-06-04 03:43:16 -0300

    Fix request attribute

--- app/feature/notes.py
+++ app/feature/notes.py
@@ -12,11 +12,11 @@ notes_router = APIRouter()
 @notes_router.get("/", response_model=List[Note])
 async def read_notes(request: Request):
     query = request.app.notes_table.select()
-    return await request.database.fetch_all(query)
+    return await request.app.database.fetch_all(query)
 
 
 @notes_router.post("/", response_model=Note)
 async def create_note(request: Request, note: NoteIn):
     query = request.app.notes_table.insert().values(text=note.text, completed=note.completed)
-    last_record_id = await request.database.execute(query)
+    last_record_id = await request.app.database.execute(query)
     return {**note.dict(), "id": last_record_id}
