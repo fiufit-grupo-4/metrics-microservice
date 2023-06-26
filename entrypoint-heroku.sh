72c1c9cede05aa97202b38a8294701bc2594d394
Federico Pacheco <fpacheco@fi.uba.ar>
2023-06-26 13:21:45 -0300

    update deploy with datadog

---
+++ entrypoint-heroku.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+datadog-agent run > /dev/null &
+/opt/datadog-agent/embedded/bin/trace-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
+/opt/datadog-agent/embedded/bin/process-agent --config=/etc/datadog-agent/datadog.yaml > /dev/null &
+poetry run uvicorn app.main:app --host 0.0.0.0 --port ${PORT}
